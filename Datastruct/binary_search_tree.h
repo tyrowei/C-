53d02988b610f3ad249fc812cb5ac444857915e8
tyrowei <weitaotyro@163.com>
2017-03-11 16:02:22 +0800

    modify binary search tree

--- Datastruct/binary_search_tree.h
+++ Datastruct/binary_search_tree.h
@@ -1,53 +1,67 @@
 #ifndef BINARYSEARCHTREE_H__
 #define BINARYSEARCHTREE_H__
 
+/*
+** 二叉查找树
+** 若它的左子树不为空，则左子树上所有结点的值都小于根结点的值
+** 若它的右子树不为空，则右子树上所有结点的值都大于根结点的值
+** 左右子树又分别是二叉查找树
+*/
+
 /**
 ** 二叉查找树的插入
 ** 类似于查找，只是当到达相应的位置为空时，建立节点
-** 如果有重复的，则可在节点中建立一个count来统计
+** 如果有重复的，则可在节点中建立一个count来统计或者不处理
 **
 ** 删除
 ** 如果是叶节点，则直接删除
-** 如果该节点还有子节点，则可用右子树中最小值来代替
+** 如果该节点还有子节点，则可用右子树中最小值或者左子树中的最大值来代替
 **/
 
 class BinarySearchTree
 {
 private:
+	//结点结构
 	struct BSNode
 	{
 		int data;
 		BSNode *left;
 		BSNode *right;
-		
-		BSNode(int val, BSNode *lt, BSNode *rt) : data(val), left(lt), right(rt) { }
+
+		BSNode(int val = 0, BSNode *lt = NULL, BSNode *rt = NULL) :
+			data(val), left(lt), right(rt) { }
 	};
-	
+
 public:
 	BinarySearchTree();
 	~BinarySearchTree();
 	BinarySearchTree(const BinarySearchTree &rhs);	//right hand side
 	BinarySearchTree &operator=(const BinarySearchTree &rhs);
-	
+
 	bool IsEmpty() const;
-	bool Contains(const int &val) const;
-	const int &FindMax() const;
-	const int &FindMin() const;
+	bool Contains(const int &val) const { return Contains(val, m_root); }
+	const int &FindMax() { return FindMax(m_root); }
+	const int &FindMin() { return FindMin(m_root); }
 	
-	void Insert(const int &val);
-	void Remove(const int &val);
+	void Insert(const int &val) { Insert(m_root, val); }
+	void Remove(const int &val) { Remove(m_root, val); }
 	
-	void PreorderPrintTree() const;
-	void InorderPrintTree() const;
-	void PostorderPrintTree() const;
+	void PreorderPrintTree() { PreorderPrintTree(m_root); }		//前序遍历 根左右
+	void InorderPrintTree() { InorderPrintTree(m_root); }		//中序遍历 左根右
+	void PostorderPrintTree() { PostorderPrintTree(m_root); }	//后序遍历 左右根
 
 private:
+	//给外部接口调用的内部接口
 	Delete(BSNode *root);
 	bool Contains(const int &val, BSNode *node) const;
 	const int &FindMax(BSNode *node) const;
 	const int &FindMin(BSNode *node) const;
 	void Insert(const int &val, BSNode *node);
 	void Remove(const int &val, BSNode *node);
+
+	void PreorderPrintTree(BSNode *node) const;
+	void InorderPrintTree(BSNode *node) const;
+	void PostorderPrintTree(BSNode *node) const;
 
 private:	
 	BSNode *m_root;	//根节点，为NULL表示是空树
@@ -63,7 +77,7 @@ BinarySearchTree::~BinarySearchTree()
 	Delete(m_root);
 }
 
-bool BinarySearchTree::IsEmpty()
+bool BinarySearchTree::IsEmpty() const
 {
 	if (NULL == m_root)
 	{
@@ -73,12 +87,7 @@ bool BinarySearchTree::IsEmpty()
 	return false;
 }
 
-bool BinarySearchTree::Contains(const int &val)
-{
-	return Contains(val, m_root);
-}
-
-bool BinarySearchTree::Contains(const int &val, BSNode *node)
+bool BinarySearchTree::Contains(const int &val, BSNode *node) const
 {
 	if (NULL == node)
 	{
@@ -98,17 +107,7 @@ bool BinarySearchTree::Contains(const int &val, BSNode *node)
 	}
 }
 
-const int &BinarySearchTree::FindMax()
-{
-	return FindMax(m_root);
-}
-
-const int &BinarySearchTree::FindMin()
-{
-	return FindMin(m_root);
-}
-
-const int &BinarySearchTree::FindMax(BSNode *node)
+const int &BinarySearchTree::FindMax(BSNode *node) const
 {
 	if (NULL == node)
 	{
@@ -121,7 +120,7 @@ const int &BinarySearchTree::FindMax(BSNode *node)
 	return node->data;
 }
 
-const int &BinarySearchTree::FindMin(BSNode *node)
+const int &BinarySearchTree::FindMin(BSNode *node) const
 {
 	if (NULL == node)
 	{
@@ -134,21 +133,11 @@ const int &BinarySearchTree::FindMin(BSNode *node)
 	return node->data;
 }
 
-void BinarySearchTree::Insert(const int &val)
-{
-	Insert(m_root, val);
-}
-
-void BinarySearchTree::Remove(const int &val)
-{
-	Remove(m_root, val);
-}
-
 void BinarySearchTree::Insert(const int &val, BSNode *node)
 {
 	if (NULL == node)
 	{
-		node = new BSNode(val, NULL, NULL);
+		node = new BSNode(val);
 	}
 	else if (val < node->data)
 	{
@@ -160,8 +149,39 @@ void BinarySearchTree::Insert(const int &val, BSNode *node)
 	}
 	else
 	{
-		//相等
+		//相等，暂不处理
 	}
 }
+
+void BinarySearchTree::PreorderPrintTree(BSNode* node)
+{
+	if (node)
+	{
+		cout << node->data << " ";
+		PreorderPrintTree(node->left);
+		PreorderPrintTree(node->right);
+	}
+}
+
+void BinarySearchTree::InorderPrintTree(BSNode* node)
+{
+	if (node)
+	{
+		PreorderPrintTree(node->left);
+		cout << node->data << " ";
+		PreorderPrintTree(node->right);
+	}
+}
+
+void BinarySearchTree::PostorderPrintTree(BSNode* node)
+{
+	if (node)
+	{
+		PreorderPrintTree(node->left);
+		PreorderPrintTree(node->right);
+		cout << node->data << " ";
+	}
+}
+
 
 #endif  /*BINARYSEARCHTREE_H__*/
